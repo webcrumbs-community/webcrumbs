6a679645e5832f65ebe1abd559859d9b8c7c395a
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-09-22 22:09:29 +0530

    that mistake fixed

--- admin/shared/layout/Layout.jsx
+++ admin/shared/layout/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<body>{children}</body>
+			{children}
 		</>
 	);
 }
