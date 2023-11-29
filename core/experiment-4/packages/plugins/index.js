11ae0457358b5650207f485b0d60142f6bf03805
webcrumbs-community <julia@juliamachado.com.br>
2023-11-28 21:20:17 -0300

    Solved issue #161

--- core/experiment-4/packages/plugins/index.js
+++ core/experiment-4/packages/plugins/index.js
@@ -8,10 +8,9 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
-app.use(limiter);
 
 // Serve the JavaScript bundle file directly
-app.get('/plugins/:pluginName', (req, res) => {
+app.get('/plugins/:pluginName', limiter, (req, res) => {
     const { pluginName } = req.params;
     res.sendFile(path.join(__dirname, '/dist', pluginName + '.js'));
 });
