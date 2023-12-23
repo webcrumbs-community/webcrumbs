46f4c7308982ea55a0e539b949e6f4fe6425929d
Wisdom-Daniel <44666155+dew-dr0p@users.noreply.github.com>
2023-12-23 10:11:17 +0100

    Modified Plugin3 to handle upload of files

--- core/experiment-6/packages/plugins/dev/plugin3/src/App.jsx
+++ core/experiment-6/packages/plugins/dev/plugin3/src/App.jsx
@@ -1,13 +1,32 @@
 import React, { useEffect, useState } from "react";
 
 export default function App(props) {
-  const [counter, setCounter] = useState(0);
+  const [fileName, setFileName] = useState('');
+  const [fileContents, setFileContents] = useState('')
+
+  const handleFileUpload = (e) => {
+    const fileUploaded = e.target.files[0]
+    console.log(fileUploaded)
+    setFileName(fileUploaded.name)
+
+    const reader = new FileReader()
+    console.log(reader)
+    reader.readAsDataURL(fileUploaded)
+    reader.onload = () => {
+      console.log(reader.result)
+      setFileContents(reader.result)
+    }
+  }
 
   useEffect(() => {
     console.log(props);
   }, []);
 
   return (
-    <button onClick={() => { setCounter(counter + 1) }}>Counter: {counter}</button>
+    <div>
+      <input type="file" name="fileInput" id="file-input" onChange={(e) => handleFileUpload(e)} />
+      <p>File Name: {fileName}</p>
+      <p>File Contents: {fileContents}</p>
+    </div>
   )
 }
