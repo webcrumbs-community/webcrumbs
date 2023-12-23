e1fa23a96bdead3e1d7d996b3d36dd9f2dd7e91f
Wisdom-Daniel <44666155+dew-dr0p@users.noreply.github.com>
2023-12-23 11:15:01 +0100

    Modified plugin3 for file conversion

--- core/experiment-6/packages/plugins/dev/plugin3/src/App.jsx
+++ core/experiment-6/packages/plugins/dev/plugin3/src/App.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
+// import { jsPDF } from 'jspdf'
+
+const jsPDF = require('jspdf')
 
 export default function App(props) {
   const [fileName, setFileName] = useState('');
   const [fileContents, setFileContents] = useState('')
+  const [isFileImage, setIsFileImage] = useState(false)
+
+  const reader = new FileReader()
+  const pdfFile = new jsPDF()
 
   const handleFileUpload = (e) => {
     const fileUploaded = e.target.files[0]
     console.log(fileUploaded)
     setFileName(fileUploaded.name)
 
-    const reader = new FileReader()
+    setIsFileImage(fileUploaded.type.startsWith('image/'))
+
     console.log(reader)
     reader.readAsDataURL(fileUploaded)
     reader.onload = () => {
@@ -18,15 +26,25 @@ export default function App(props) {
     }
   }
 
+  const handleFileConversion = (imageData) => {
+    pdfFile.addImage(imageData, 'JPEG', 10, 10)
+  }
+
   useEffect(() => {
     console.log(props);
   }, []);
 
   return (
     <div>
+      <h3>This plugin is aimed at testing how to convert an image uploaded to pdf and then downloading the saved pdf.</h3>
+      <label htmlFor="file-input">Click the button to upload image</label>
       <input type="file" name="fileInput" id="file-input" onChange={(e) => handleFileUpload(e)} />
       <p>File Name: {fileName}</p>
-      <p>File Contents: {fileContents}</p>
+
+      <button onClick={() => handleFileConversion(fileContents)}>Click to convert to pdf</button>
+      {!isFileImage && <h5>Uploaded file is not an Image</h5>}
+
+      {isFileImage && <button onClick={() => pdfFile.save(`${fileName.split('.')[0]}.pdf`)}>Click to download pdf</button>}
     </div>
   )
 }
