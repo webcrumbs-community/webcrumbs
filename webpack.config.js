8f06ad01d5a21afafeb5030e44691e6fc4af8f88
webcrumbs-community <julia@juliamachado.com.br>
2024-01-06 16:37:00 -0300

    Reorganized

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "/src/admin/admin-panel.js", // The entry point of your admin panel code.
-  output: {
-    filename: "admin-panel.js",
-     path: path.resolve(__dirname, 'demo'), // Output to the /demo directory.
-  },
-};
